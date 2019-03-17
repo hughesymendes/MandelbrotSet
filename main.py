eca0e564ef9e9de206c36fcbed4d6aa7ae4af4ee
hughesymendes <hughesy_94@hotmail.co.uk>
2019-03-17 19:44:51 +0000

    Update main.py

--- main.py
+++ main.py
@@ -9,29 +9,22 @@ def calculate_if_is_in_mbot_set(Comp):
 	Z=np.zeros(C.shape, np.complex64)
 	Re = np.linspace(xmin, xmax, xn, dtype=np.float32)
 	Imag = np.linspace(ymix, ymax, yn, dtype=np.float32)
-        C = Re + Imag
+        C = (Re + Imag)
         C**2 = Re**2 - Imag**2 + 2*Re*Imag
+        Z = 0.0
+        C = -1
         maxiter = 100
 	while Comp <= maxiter:
                 xmin, xmax, xn = -2.25, +0.75, 3000/2
                 ymin, ymax, yn = -1.25, +1.25, 2500/2
                 
-                Z = 0.0
-                
-                C = -1
-
                 Z = Z**2 + C
-
                 Z = C**2 + C
-
-                C**2 = (C**2 + C) 
-                
-                Z = C**2 + C 
-                
+                C**2 = (C**2 + C)
+                Z = C**2 + C
 		return True
 	else:
                 break
-        
                 return False
 
 
@@ -42,11 +35,7 @@ def visualise_answer( is_in_set ):
 		 print str(Comp) + " Is in the Mandelbrot set"
 	else:
                  print str(Comp) + " Is not in the Mandelbrot set"
-
-
-
-
-
+                 
 while Comp <= maxiter:
 
   is_in_mbot_set = calculate_if_is_in_mbot_set( Comp )
